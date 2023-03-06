09ba7c3b1b731fd0fb8ecbf7d9fbb7b233a7976f
DavidHunterScot <david.hunter@echoverse.cc>
2023-03-06 16:16:00 +0000

    Added remaining clips from current archive for BBC Radio 4.

---
+++ src/bbc-radio-4/BBC-Radio-4---HD---2-Feb-2021---Midnight-GMT.php
@@ -0,0 +1,20 @@
+<?php
+
+$current_nav_item = "bbc-radio-4";
+
+$clip_slug = substr( basename( __FILE__ ), 0, -4 );
+
+$clip_title = str_replace( array( "-", "   " ), array( " ", " - " ), $clip_slug );
+
+$clip_url = "/bbc-radio-4/" . $clip_slug;
+
+$clip_direct_url = "https://storage.analogueradio.ict.rocks/bbc-radio-4/" . $clip_slug . ".mp4";
+
+$clip_thumbnail_url = "/assets/images/" . $clip_slug . ".png";
+
+$clip_duration = "0:47";
+
+if( isset( $path_to_input_file ) && basename( $path_to_input_file ) == basename( __FILE__ ) )
+{
+    include __DIR__ . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . "_includes" . DIRECTORY_SEPARATOR . "clip-player.php";
+}
